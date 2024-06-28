edaf739b6cf2427daac530df7fe89a2e6692b436
raonayonika <162417341+raonayonika@users.noreply.github.com>
2024-06-28 11:33:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='example',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'numpy',
+    ],
+    entry_points={
+        'console_scripts': [
+            'example=example.my_project.module:main',
+        ],
+    },
+    author='Nayonika Rao',
+    author_email='raonayonika@gmail.com',
+    description='An example project to demonstrate documentation ',
+    url= 'https://github.com/raonayonika/example',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
 
