d7008d172819530a3c7c8f1d1cda83fd4a8a540e
raonayonika <162417341+raonayonika@users.noreply.github.com>
2024-07-01 10:13:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='example',
-    version='0.1.0',
-    packages=find_packages(),
+    name='example',  # The name of the package
+    version='0.1.0',  # The initial release version
+    packages=find_packages(),  # Automatically find and include all packages in the project
     install_requires=[
-        'requests',
-        'numpy',
+        'requests',  # External package dependency
+        'numpy',  # Another external package dependency
     ],
     entry_points={
         'console_scripts': [
-            'example=example.my_project.module:main',
+            'example=example.my_project.module:main',  # Command-line script entry point
         ],
     },
-    author='Nayonika Rao',
-    author_email='raonayonika@gmail.com',
-    description='An example project to demonstrate documentation ',
-    url= 'https://github.com/raonayonika/example',
+    author='Nayonika Rao',  # Author's name
+    author_email='raonayonika@gmail.com',  # Author's email
+    description='An example project to demonstrate documentation',  # Short project description
+    url='https://github.com/raonayonika/example',  # URL for the project homepage
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',  # Python version compatibility
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',  # OS compatibility
     ],
 )
 
